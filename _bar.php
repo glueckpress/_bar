d01ad41db79771b7cecb7cd04f10459985b2b9a7
Caspar Hübinger <caspar@glueckpress.com>
2015-08-06 16:43:54 +0200

    modulized functionality

---
+++ _bar.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name: _bar
+ * Description: Nifty hacks for the WordPress admin toolbar (aka admin bar).
+ * Version:     0.1
+ * Author:      Caspar Hübinger
+ * Plugin URI:  https://github.com/glueckpress/_bar/
+ * Author URI:  https://profiles.wordpress.org/glueckpress
+ * License: GNU General Public License v3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: _bar
+ * Domain Path: /languages
+ */
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+/**
+ * Initialize plugin.
+ *
+ * @return void
+ */
+function _bar__load() {
+
+	// Admins only.
+	if ( ! current_user_can( 'manage_options' ) )
+		return;
+
+	$plugin_slug = plugin_basename( __FILE__ );
+	load_plugin_textdomain(
+		'_bar',
+		false,
+		dirname( $plugin_slug ) . '/languages/'
+	);
+
+	/**
+	 * Modules
+	 */
+	$module_dir = dirname( __FILE__ ) . '/inc';
+
+	// Site Language
+	require_once( $module_dir . '/site-language/site-language.php' );
+
+}
+add_action( 'plugins_loaded', '_bar__load' );
+
